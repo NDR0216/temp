a9657d406907ce48cc0c33a8d34d7218c734659e
Ander Liu <anderliu0216@gmail.com>
2024-11-15 05:39:54 -0600

    s

--- push_with_tests.sh
+++ push_with_tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Check if a branch name was provided
+if [ -z "$1" ]; then
+  echo "Please provide a branch name."
+  exit 1
+fi
+
 # Check if a commit is ready to be pushed
 if [ -z "$(git status --porcelain)" ]; then
   # Run the tests and store the output
-  test_output=$(npm test)
+  test_output=$(npm test 2>&1)
 
   # Check if the tests were successful
   if [ $? -eq 0 ]; then
